6df594dfeffef1f38d75708f137e4f50d3fa3e9a
Fredrik Aurdal <fredrik.a@protonmail.ch>
2019-08-24 21:57:32 +0200

    Country pages work

--- front-end/country-app/src/App.js
+++ front-end/country-app/src/App.js
@@ -5,12 +5,14 @@ import './App.css';
 // Components
 import Home from './components/Home';
 import Countries from './components/Countries';
+import Country from './components/Country';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/countries" component={Countries} />
+      <Route exact path="/countries/:code" component={Country} />
     </Switch>
   );
 }
---
+++ front-end/country-app/src/components/Country.js
@@ -0,0 +1,40 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+
+import { Query } from 'react-apollo';
+import { gql } from 'apollo-boost';
+
+const Country = ({ match }) => (
+  <Query
+    query={gql`
+      {
+        country(code: "${match.params.code}") {
+          name
+          native
+          currency
+          phone
+        }
+      }
+    `}
+  >
+    {({ loading, error, data }) => {
+      if (loading) return <h1>Loading...</h1>;
+      if (error) return <h2>404 - The API is down</h2>;
+
+      const { name, native, currency, phone } = data.country;
+
+      return (
+        <Fragment>
+          <p>Name (English): {name}</p>
+          <p>Name (Native): {native}</p>
+          <p>Currency: {currency}</p>
+          <p>Phone Counry Code: +{phone}</p>
+        </Fragment>
+      );
+    }}
+  </Query>
+);
+
+Country.propTypes = {};
+
+export default Country;
